fed966c30490f30d375e3aa9d5d31363e0f31c8d
gitdaCode <nirajbadaik@yahoo.com>
2021-07-27 00:30:09 +0530

    Add code

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{data ? `${data.message}` : "Loading..."}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
