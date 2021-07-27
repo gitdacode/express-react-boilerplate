49938d34838c19f23a32195f23c44a669e764083
NirajBadaik <61387183+NirajBadaik@users.noreply.github.com>
2021-07-27 19:35:34 +0530

    Refactor: MVC pattern (#1)
    * Add MVC with mongodb connection.
    
    * Add .env.example

---
+++ server/models/model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+const Model = mongoose.model("model", schema);
+
+module.exports = Model;
